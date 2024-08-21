e2122464be1f76de3557853642265d52449e9ed2
steven aditya <stv_Aditya@yahoo.com>
2024-08-22 00:58:29 +0700

    Login edited

--- app/(auth)/index.jsx
+++ app/(auth)/index.jsx
@@ -1,11 +1,42 @@
 import {
     View, Text, Image, TextInput,
-    Button, StyleSheet
+    Button, StyleSheet, Alert
 } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, router } from 'expo-router';
 
 export default function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSignIn = () => {
+        if (!email || !password) {
+            Alert.alert('Error', 'Please fill out all fields.');
+            return;
+        }
+        if (!email.includes('@')) {
+            Alert.alert('Error', 'Please enter a valid email address.');
+            return;
+        }
+        if (password.length < 6) {
+            Alert.alert('Error', 'Password must be at least 6 characters long.');
+            return;
+        }
+        if (password.includes(' ')) {
+            Alert.alert('Error', 'Password cannot contain spaces.');
+            return;
+        }
+        const specialCharPattern = /[!@#$%^&*(),.?":{}|<>]/;
+        if (!specialCharPattern.test(password)) {
+            Alert.alert('Error', 'Password must contain at least one special character.');
+            return;
+        }
+
+
+        router.navigate('../(tabs)');
+        Alert.alert('Sign In Succesfully!');
+    };
     return (
         <View>
             <Image style={styles.logo} source={require('@/assets/images/tmmin.png')} />
@@ -14,7 +45,11 @@ export default function Login() {
                 <Text style={styles.formLabel}>Email</Text>
                 <TextInput
                     style={styles.formInput}
-                    placeholder='johndee@gmail.com' />
+                    placeholder='johndee@gmail.com'
+                    value={email}
+                    onChangeText={setEmail}
+                    accessibilityLabel="Email"
+                />
             </View>
             <View style={styles.formContainer}>
                 <Text style={styles.formLabel}>Password</Text>
@@ -22,13 +57,17 @@ export default function Login() {
                     style={styles.formInput}
                     secureTextEntry={true}
                     placeholder='password'
+                    value={password}
+                    onChangeText={setPassword}
+                    accessibilityLabel="Password"
                 />
             </View>
             <View style={styles.formContainer}>
                 <Button
-                    onPress={() => router.navigate('../(tabs)')}
+                    onPress={handleSignIn}
                     color='#3D7B3F'
-                    title="Sign In" />
+                    title="Sign In"
+                    accessibilityLabel='Sign In' />
                 <Text style={styles.textRegister}>
                     Don't have an account?{` `}
                     <Link
