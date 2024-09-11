b7bf80492da0bb10f264a0d591fd61c7f20552d3
steven aditya <stv_Aditya@yahoo.com>
2024-09-11 23:24:23 +0700

    latest

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
+  presets: ['module:metro-react-native-babel-preset'];
+  plugins: ['react-native-reanimated/plugin'];
   return {
     presets: ['babel-preset-expo'],
   };
